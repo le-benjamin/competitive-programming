9af65c2dccd018484b446623e032021e9e887d47
Ben Le <benjamin.d.le@protonmail.com>
2022-05-14 21:03:22 -0500

    Add subset.cpp

---
+++ USACO/Training/Chapter_2/subset.cpp
@@ -0,0 +1,119 @@
+/*
+ID: b.le611
+PROG: subset
+LANG: C++14
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define ar array
+
+#define vt vector
+#define pb push_back
+#define eb emplace_back
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+
+#define f first
+#define s second
+
+#define CSTR(n) ((char*)(malloc(sizeof(char)*n)))
+
+#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
+#define EACH(x, a) for (auto& x: a)
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+
+void setIO(string name = "", int prec = 2) { // lim prec = 15
+    cin.tie(nullptr)->sync_with_stdio(0);
+    cout << fixed << setprecision(prec);
+    if (sz(name)) {
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout); }
+}
+
+string to_string(char c) { return string(1, c); }
+string to_string(bool b) { return b?"true":"false"; }
+string to_string(const char* s) { return string(s); }
+string to_string(string s) { return s; }
+template<class T> string to_string(T v) {
+    bool f=1;
+    string res;
+    EACH(x, v) {
+        if(!f) res+=' ';
+        f=0;
+        res+=to_string(x);
+    }
+    return res;
+}
+
+template<class T> void read(T& x) { cin >> x; }
+void read(double& d) { string t; read(t); d=stod(t); } // db & ld are special
+void read(ld& d) { string t; read(t); d=stold(t); }
+template<class H, class... T> void read(H& h, T&... t) { read(h); read(t...); }
+template<class A> void read(vt<A>& x) { EACH(a, x) read(a); }
+
+template<class A> void write(A x) { cout << to_string(x); }
+template<class H, class... T> void write(const H& h, const T&... t) { write(h); write(t...); }
+void print() { write("\n"); }
+template<class H, class... T> void print(const H& h, const T&... t) { 
+    write(h);
+    if(sizeof...(t))
+        write(' ');
+    print(t...);
+}
+
+// task variables, funcs, definitions
+#define MAX_N 40
+int N;
+
+// bottom up dp
+int solve(int total, int lim) {
+    total++, lim++; // just makes typing easier
+    vt<vt<int>> dp(total, vt<int>(lim, 0));
+    
+    // init
+    FOR(i, lim)
+        dp[i][i] = 1;
+
+    FOR(i, 3, total) {
+        for (int j = 1; j < lim && j < i; j++) {
+            int count = 0;
+            FOR(k, j)
+                count += dp[i-j][k];
+            dp[i][j] = count;
+        }
+    }
+
+    int count = dp[total-1][lim-1];
+    return count;
+}
+
+int main() {
+    setIO("subset");
+
+    read(N);
+    int goal = N*(N+1)/2;
+
+    if (goal&1)
+        print(0);
+    else {
+        goal/=2;
+        int count = solve(goal, N);
+        print(count);
+    }
+
+    return 0;
+}
