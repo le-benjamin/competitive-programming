ed7ee9823cbcfbfef8b186eb7392de55a6abb925
Ben Le <benjamin.d.le@protonmail.com>
2022-05-15 14:56:24 -0500

    Update jtemplate.java

--- jtemplate.java
+++ jtemplate.java
@@ -3,10 +3,37 @@ import java.util.*;
 
 public class jtemplate {
     public static void main(String[] args) throws IOException {
-        // keep template simple for use at UIL
-        Scanner in = new Scanner(new File("jtemplate.dat"));
+        Kattio io = new Kattio();
         
         
-        in.close();
+        io.close();
     }
+
+    static class Kattio extends PrintWriter {
+		private BufferedReader r;
+		private StringTokenizer st;
+        
+		public Kattio() { this(System.in, System.out); }
+		public Kattio(InputStream i, OutputStream o) {
+			super(o);
+			r = new BufferedReader(new InputStreamReader(i));
+		}
+        
+		public Kattio(String problemName) throws IOException {
+			super(problemName + ".out");
+			r = new BufferedReader(new FileReader(problemName + ".in"));
+		}
+        
+		public String next() {
+			try {
+				while (st == null || !st.hasMoreTokens())
+					st = new StringTokenizer(r.readLine());
+				return st.nextToken();
+			} catch (Exception e) { }
+			return null;
+		}
+		public int nextInt() { return Integer.parseInt(next()); }
+		public double nextDouble() { return Double.parseDouble(next()); }
+		public long nextLong() { return Long.parseLong(next()); }
+	}
 }
