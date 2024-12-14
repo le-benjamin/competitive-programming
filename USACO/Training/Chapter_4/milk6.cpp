c6f7fabb9a763664d5401269bde2b9cb9a2acb8d
Ben Le <benjamin.d.le@protonmail.com>
2024-12-13 21:23:58 -0600

    Add milk6.cpp

--- USACO/Training/Chapter_4/milk6.cpp
+++ USACO/Training/Chapter_4/milk6.cpp
@@ -39,7 +39,7 @@ int maxFlow(int s, int e, int g[32][32]) {
     int pre[32]; 
     bool vis[32];
     int flow[32];
-    int ret = 0;
+    int totalFlow = 0;
     int pathcap = 0;
     for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 32; j++) {
@@ -50,7 +50,8 @@ int maxFlow(int s, int e, int g[32][32]) {
     while (1) {
         fill(pre, pre+32, -1);
         fill(vis, vis+32, false);
-        flow[0] = MOD;
+        fill(flow, flow+32, 0);
+        flow[s] = MOD;
         
         int mf = 0;
         int mfi = -1;
@@ -64,12 +65,14 @@ int maxFlow(int s, int e, int g[32][32]) {
                     mfi = i;
                 }
             }
-            if (mfi = -1) {
+
+            if (mfi == -1) {
                 break;
             }
-            if (mfi = 1) {
+            if (mfi == e) {
                 break;
             }
+
             vis[mfi] = true;
             for (int i = 0; i < n; i++) {
                 if (h[mfi][i] == 0) {
@@ -82,15 +85,14 @@ int maxFlow(int s, int e, int g[32][32]) {
             }
         }
 
-        if (mfi = -1) {
+        if (mfi == -1) {
             break;
         }
+        pathcap = flow[e];
+        totalFlow += pathcap;
 
-        pathcap = flow[0];
-        ret += pathcap;
-
-        int curi = n-1;
-        while (curi != 0) {
+        int curi = e;
+        while (curi != s) {
             int nexi = pre[curi];
             h[nexi][curi] -= pathcap;
             h[curi][nexi] += pathcap;
@@ -98,7 +100,7 @@ int maxFlow(int s, int e, int g[32][32]) {
         }
     }
     
-    return ret;
+    return totalFlow;
 }
 
 int main() {
@@ -107,19 +109,42 @@ int main() {
     cin >> n >> m;
 
     int g[32][32];
+    fill(&g[0][0], &g[0][0]+32*32, 0);
     vector<pair<int, pair<int, pair<int, int>>>> edges;
     for (int i = 0; i < m; i++) {
         int s, e, c;
         cin >> s >> e >> c;
         s--;
         e--;
-        g[s][e] = c;
-        edges.push_back(pair<int, pair<int, pair<int, int>>>(c, pair<int, pair<int, int>>(i, pair<int,int>(s, e))));
+        g[s][e] += c;
+        edges.push_back(pair<int, pair<int, pair<int, int>>>(-c, pair<int, pair<int, int>>(i, pair<int,int>(s, e))));
     }
 
     sort(edges.begin(), edges.end());
+    vector<int> routes;
+    int mf = maxFlow(0, n-1, g), C = 0;
+    for (int i = 0; i < m; i++) {
+        int s, e, c, ri;
+        s = edges[i].second.second.first;
+        e = edges[i].second.second.second;
+        c = -edges[i].first;
+        ri = edges[i].second.first;
+        g[s][e] -= c;
+        int f = maxFlow(0, n-1, g);
+        if (mf-f == c) {
+            C += c;
+            mf = f;
+            routes.push_back(ri);
+        } else {
+            g[s][e] += c;
+        }
+    }
 
-
+    cout << C << " " << routes.size() << "\n";
+    sort(routes.begin(), routes.end());
+    for (auto ri : routes) {
+        cout << ri+1 << '\n';
+    }
 
     return 0;
 }
